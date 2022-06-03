a9a322ade4edc29d7d0300941f4f633f35f3df59
Stephen Lineker-Miller <slinekermiller@gmail.com>
2022-06-03 21:02:31 +0100

    refactor: remove legacy docker file

--- dockerfiles/update-docker.sh
+++ dockerfiles/update-docker.sh
@@ -1,6 +1,3 @@
-# echo "Creating Docker Image"
-# docker build -t 'virtual_machine' - < Dockerfile
-#
 echo "Creating Docker Image - Python"
 docker build -t 'virtual_machine_python' - < DockerFilePython
 
