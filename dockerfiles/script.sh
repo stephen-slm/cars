82c981592f55a521db69689e6ad37971cf5bc54e
Stephen Lineker-Miller <slinekermiller@gmail.com>
2022-06-03 16:17:47 +0100

    feat: script now outputs in micoseconds compile and runtime

--- dockerfiles/script.sh
+++ dockerfiles/script.sh
@@ -18,25 +18,25 @@ COMPILE_START=0
 COMPILE_END=0
 
 if [ "$output" = "" ]; then
-  RUNTIME_START=$(date +%s.%N)
+  RUNTIME_START=$(date +%s%3N)
   $compiler "$sourceFile" - <"${stdInFile}"
-  RUNTIME_END=$(date +%s.%N)
+  RUNTIME_END=$(date +%s%3N)
 else
-  COMPILE_START=$(date +%s.%N)
+  COMPILE_START=$(date +%s%3N)
   $compiler "$sourceFile" "$additionalArguments"
-  COMPILE_END=$(date +%s.%N)
+  COMPILE_END=$(date +%s%3N)
 
   if [ $? -eq 0 ]; then
-    RUNTIME_START=$(date +%s.%N)
+    RUNTIME_START=$(date +%s%3N)
     $output - <"${stdInFile}"
-    RUNTIME_END=$(date +%s.%N)
+    RUNTIME_END=$(date +%s%3N)
   else
     echo "Compilation Failed"
   fi
 fi
 
 
-runtime=$(echo "$RUNTIME_END - $RUNTIME_START" | bc)
-compile=$(echo "$COMPILE_END - $COMPILE_START" | bc)
+runtime=$(expr $RUNTIME_END - $RUNTIME_START)
+compile=$(expr $COMPILE_END - $COMPILE_START)
 
 echo "*-COMPILE::EOF-*" "$runtime" "$compile"
