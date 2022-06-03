29c3cac391c0178e00f32767453c9d3e94ecc991
Stephen Lineker-Miller <slinekermiller@gmail.com>
2022-06-03 09:47:50 +0100

    feat: execution script now times compiling

--- dockerfiles/script.sh
+++ dockerfiles/script.sh
@@ -11,22 +11,32 @@ standard_error_out=$7
 exec 1>"${standard_out}"
 exec 2>"${standard_error_out}"
 
-START=$(date +%s.%2N)
+RUNTIME_START=0
+RUNTIME_END=0
+
+COMPILE_START=0
+COMPILE_END=0
 
 if [ "$output" = "" ]; then
+  RUNTIME_START=$(date +%s.%2N)
   $compiler "$sourceFile" - <"${stdInFile}"
+  RUNTIME_END=$(date +%s.%2N)
 else
+  COMPILE_START=$(date +%s.%2N)
   $compiler "$sourceFile" "$additionalArguments"
+  COMPILE_END=$(date +%s.%2N)
 
   if [ $? -eq 0 ]; then
+    RUNTIME_START=$(date +%s.%2N)
     $output - <"${stdInFile}"
+    RUNTIME_END=$(date +%s.%2N)
   else
     echo "Compilation Failed"
   fi
 fi
 
 
-END=$(date +%s.%2N)
-runtime=$(echo "$END - $START" | bc)
+runtime=$(echo "$RUNTIME_END - $RUNTIME_START" | bc)
+compile=$(echo "$COMPILE_END - $COMPILE_START" | bc)
 
-echo "*-COMPILE::EOF-*" "$runtime"
+echo "*-COMPILE::EOF-*" "$runtime" $"$compile"
