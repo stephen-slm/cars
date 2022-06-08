8a5e474a1ead47ebab139c6d93e808dfe10abfe6
Stephen Lineker-Miller <stephen.lineker-miller@deliveroo.co.uk>
2022-06-08 08:55:39 +0100

    feat: template out files config struct

--- internal/files/files.go
+++ internal/files/files.go
@@ -4,3 +4,10 @@ type Files interface {
 	WriteFile(id string, name string, data []byte) error
 	GetFile(id string, name string) ([]byte, error)
 }
+
+type FilesConfig struct {
+	// LocalRootPath if set will be used as the directory the source and the output
+	// will be written into if its in local mode. If this is not set and local mode
+	// is enabled then this will be the temp directory of the machine (os.TempDir())
+	LocalRootPath string
+}
