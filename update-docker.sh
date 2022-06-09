be3b347f444300b15d4d61bfbd5728d3ecbf6e8c
Stephen Lineker-Miller <slinekermiller@gmail.com>
2022-06-09 21:46:49 +0100

    feat: invert docker names to not have language name as extension but dockerfile

--- update-docker.sh
+++ update-docker.sh
@@ -5,14 +5,15 @@ GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 for FILE in ./build/dockerfiles/*; do
-  echo "${GREEN}Creating Docker Image${ENDCOLOR} - ${RED}${FILE##*dockerfiles\/}${ENDCOLOR}"
+  FILE_NAME=${FILE##*dockerfiles\/}
+  LANG=${FILE_NAME%%.*}
+
+  echo "${GREEN}Creating Docker Image${ENDCOLOR} - ${RED}${LANG}${ENDCOLOR}"
 
   if [ -z "$1" ]
-    then
-      docker build -f $FILE -t "virtual_machine_${FILE##*dockerfiles\/Dockerfile.}" . > /dev/null
-    else
-      docker build --progress=plain -f $FILE -t "virtual_machine_${FILE##*dockerfiles\/Dockerfile.}" .
+    then docker build -f $FILE -t "virtual_machine_${LANG}" . > /dev/null
+    else docker build --progress=plain -f $FILE -t "virtual_machine_${LANG}" .
   fi
-  echo "${GREEN}Completed Building Docker Image${ENDCOLOR} - ${RED}${FILE##*dockerfiles\/}${ENDCOLOR}"
+  echo "${GREEN}Completed Building Docker Image${ENDCOLOR} - ${RED}${LANG}${ENDCOLOR}"
 done
 
