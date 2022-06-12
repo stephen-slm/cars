ffbd640f383de3c37e4ecce5119193207a77e3c1
Stephen Lineker-Miller <slinekermiller@gmail.com>
2022-06-12 15:14:13 +0100

    feat: api to gather all supported languages

--- internal/routing/templates.go
+++ internal/routing/templates.go
@@ -1,7 +1,10 @@
 package routing
 
 import (
+	"fmt"
 	"net/http"
+	"sort"
+	"strings"
 
 	"github.com/gorilla/mux"
 
@@ -24,4 +27,36 @@ func HandleGetLanguageTemplate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_, _ = w.Write([]byte(template))
+}
+
+type supportedLanguages struct {
+	// The language code send during the compile request, this is not the same as
+	// the display name. This is also the code used to get the template.
+	LanguageCode string `json:"language_code"`
+	// The display name the user can be shown and will understand for example
+	// the display name could be C# and the code would be csharp.
+	DisplayName string `json:"display_name"`
+}
+
+func HandleListLanguagesSupported(w http.ResponseWriter, _ *http.Request) {
+	supported := make([]supportedLanguages, 0, len(sandbox.Compilers))
+
+	for langCode, compiler := range sandbox.Compilers {
+		supportedLang := supportedLanguages{
+			LanguageCode: langCode,
+			DisplayName:  compiler.Language,
+		}
+
+		if compiler.Compiler != "" && !strings.EqualFold(compiler.Compiler, langCode) {
+			supportedLang.DisplayName = fmt.Sprintf("%s (%s)", compiler.Language, compiler.Compiler)
+		}
+
+		supported = append(supported, supportedLang)
+	}
+
+	sort.Slice(supported, func(i, j int) bool {
+		return supported[i].DisplayName < supported[j].DisplayName
+	})
+
+	handleJSONResponse(w, supported, http.StatusOK)
 }
