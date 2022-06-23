44dd70b5b1e6e0a25497f3b76aa8f7711836e8c4
Stephen Lineker-Miller <stephen.lineker-miller@deliveroo.co.uk>
2022-06-23 09:17:08 +0100

    feat: include a editor, config and output with auto replacement templates

--- assets/sample-site/index.js
+++ assets/sample-site/index.js
@@ -1,20 +1,86 @@
-let editor = ace.edit('editor');
-editor.setTheme('ace/theme/dracula');
-editor.session.setMode('ace/mode/markdown');
-editor.session.setUseWrapMode(true);
-editor.container.style.lineHeight = 1.15;
-editor.renderer.setOptions({
-    showPrintMargin: false,
-    fontSize: '.875rem',
-    fontFamily: '"IBM Plex Mono", monospace'
-});
-editor.clearSelection();
+const state = {
+    editor: ace.edit('editor'),
 
-editor.session.on('change', () => {
-    document.querySelector("#preview").innerHTML = editor.getValue()
-});
+    languageAceMapping: {
+        "python2": "python",
+        "c": "c_cpp",
+        "cpp": "c_cpp",
+        "node": "javascript",
+        "fsharp": "fsharp",
+    }
+}
 
-document.querySelector('#ace-mode').addEventListener("change", function (event) {
-    editor.getSession().setMode(`ace/mode/${event.target.value.toLowerCase()}`);
-});
+/////////////////////////////////////////////////////////////
+// EVENT CALLS
+/////////////////////////////////////////////////////////////
 
+async function handleLanguageSelectionChange(event) {
+    return await updateEditorForLanguage(event.target.value.toLowerCase())
+}
+
+function handleEditorValueChange() {
+    document.querySelector("#compiler-output").innerHTML = state.editor.getValue()
+}
+
+/////////////////////////////////////////////////////////////
+// EDITOR ACTIONS
+/////////////////////////////////////////////////////////////
+
+async function updateEditorForLanguage(language) {
+    const mappingValue = state.languageAceMapping[language] || language
+
+    const template = await getLanguageTemplateValue(language)
+
+    state.editor.getSession().setMode(`ace/mode/${mappingValue}`);
+    state.editor.getSession().setValue(template)
+}
+
+function configureEditor() {
+    state.editor.setTheme('ace/theme/dracula');
+    state.editor.session.setUseWrapMode(true);
+
+    state.editor.renderer.setOptions({
+        showPrintMargin: true,
+    });
+
+    state.editor.session.on('change', handleEditorValueChange);
+
+    document.querySelector('#language-modes').addEventListener("change", handleLanguageSelectionChange);
+}
+
+
+/////////////////////////////////////////////////////////////
+// API CALLS
+/////////////////////////////////////////////////////////////
+
+async function getLanguageTemplateValue(language) {
+    const response = await fetch(`http://localhost:8080/languages/${language}/template`)
+   return await response.text()
+}
+
+
+async function getCurrentLanguageSupport() {
+    const response = await fetch('http://localhost:8080/languages')
+    const json = await response.json()
+
+    state.languages = json
+    const root = document.getElementById("language-modes")
+
+    for (const language of json) {
+        const newOption = document.createElement("option");
+        newOption.value = language['language_code']
+        newOption.innerText = language['display_name']
+
+        root.appendChild(newOption)
+    }
+}
+
+async function init() {
+    configureEditor()
+
+    await getCurrentLanguageSupport()
+    await updateEditorForLanguage(state.languages[0]["language_code"])
+}
+
+
+window.addEventListener("load", init)
