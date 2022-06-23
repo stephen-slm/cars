f2f59fd7a7ee0f9f99ae9ef5883437921510cc28
Stephen Lineker-Miller <slinekermiller@gmail.com>
2022-06-23 22:09:38 +0100

    feat: run button to trigger a compile to the backend

--- assets/sample-site/index.js
+++ assets/sample-site/index.js
@@ -22,11 +22,26 @@ function handleEditorValueChange() {
     document.querySelector("#compiler-output").innerHTML = state.editor.getValue()
 }
 
+async function handleCompileRequest(event) {
+    event.target.disabled = true
+
+    const code = state.editor.getValue();
+    const language = state.currentLanguage
+
+    const id = await compileEditorCode(language, code, [], [])
+    console.log(`language: ${language} - id: ${id}`)
+
+    event.target.disabled = false
+}
+
+
 /////////////////////////////////////////////////////////////
 // EDITOR ACTIONS
 /////////////////////////////////////////////////////////////
 
 async function updateEditorForLanguage(language) {
+    state.currentLanguage = language;
+
     const mappingValue = state.languageAceMapping[language] || language
 
     const template = await getLanguageTemplateValue(language)
@@ -38,6 +53,8 @@ async function updateEditorForLanguage(language) {
 function configureEditor() {
     state.editor.setTheme('ace/theme/dracula');
     state.editor.session.setUseWrapMode(true);
+
+    state.editor.setFontSize(16)
 
     state.editor.renderer.setOptions({
         showPrintMargin: true,
@@ -53,9 +70,35 @@ function configureEditor() {
 // API CALLS
 /////////////////////////////////////////////////////////////
 
+/**
+ * Execute a compile request to the backend, returning an id value used to gather
+ * updates regarding  the process.
+ *
+ * @param language
+ * @param code
+ * @param stdIn
+ * @param expectedStdOut
+ * @returns {Promise<string>} The id of the execution
+ */
+async function compileEditorCode(language, code, stdIn = [], expectedStdOut = []) {
+    const response = await fetch('http://localhost:8080/compile', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            language,
+            "source_code": code,
+            "stdin_data": stdIn,
+            "expected_stdout_data": expectedStdOut
+        })
+    })
+
+    const {id} = await response.json()
+    return id
+}
+
 async function getLanguageTemplateValue(language) {
     const response = await fetch(`http://localhost:8080/languages/${language}/template`)
-   return await response.text()
+    return await response.text()
 }
 
 
@@ -80,6 +123,9 @@ async function init() {
 
     await getCurrentLanguageSupport()
     await updateEditorForLanguage(state.languages[0]["language_code"])
+
+    // setup event handlers
+    document.getElementById("run").addEventListener("click", handleCompileRequest)
 }
 
 
